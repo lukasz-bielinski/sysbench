d2309378b9fbe79d10becc45d306b0c9b0f40326
pulse <antynomia@gmail.com>
2016-11-08 19:55:33 +0100

    modified:   script.sh

--- script.sh
+++ script.sh
@@ -15,7 +15,9 @@ trap cleanup SIGINT SIGTERM
 ###run provisioning  process
 while [ 1 ]
 do
-    sleep 60
+    sleep 10
+    date=$(date --iso-8601=seconds)
+    echo "starting sysbench at $date"
     sysbench --test=cpu --cpu-max-prime=20000 --num-threads=1 run
 
 
